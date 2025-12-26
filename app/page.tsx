e01d5dbb607b440b8980124023842307d3f3ab51
Oscar Lagatta <oscarlagatta@gmail.com>
2025-12-26 11:36:00 +0000

    assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 const Page = () => {
-    return (
-        <div>
-          <h1 className="text-3xl">Welcome to the Ultimate Next.js training!</h1>
-        </div>
-    )
-}
-export default Page
+  return (
+    <>
+      <div>
+        <h1 className="h1-bold">Welcome to the Ultimate Next.js training!</h1>
+      </div>
+    </>
+  );
+};
+export default Page;
